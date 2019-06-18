1c08ea6fa0e7f94d8ead4be5d4d9cbbd65b27df8
idkhromov <i.d.khromov@gmail.com>
2019-06-18 22:45:19 +0300

    cfg

--- tu_set/Main.Designer.cs
+++ tu_set/Main.Designer.cs
@@ -28,26 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.ctlMeters1 = new Drivers.LibMeter.ctlMeters();
             this.ctlConnectionSettings1 = new PollingLibraries.LibPorts.CtlConnectionSettings();
+            this.ctlMeters1 = new Drivers.LibMeter.ctlMeters();
             this.SuspendLayout();
-            // 
-            // ctlMeters1
-            // 
-            this.ctlMeters1.AddressMeter = null;
-            this.ctlMeters1.AddressParam = 0;
-            this.ctlMeters1.ChannelParam = 0;
-            this.ctlMeters1.EnableAuxilary = false;
-            this.ctlMeters1.EnableCurrent = false;
-            this.ctlMeters1.Enabled = false;
-            this.ctlMeters1.EnableDaily = false;
-            this.ctlMeters1.EnableHalfs = false;
-            this.ctlMeters1.EnableMonthly = false;
-            this.ctlMeters1.Location = new System.Drawing.Point(308, 12);
-            this.ctlMeters1.Name = "ctlMeters1";
-            this.ctlMeters1.PasswordMeter = null;
-            this.ctlMeters1.Size = new System.Drawing.Size(796, 268);
-            this.ctlMeters1.TabIndex = 1;
             // 
             // ctlConnectionSettings1
             // 
@@ -55,18 +38,35 @@
             this.ctlConnectionSettings1.COM = null;
             this.ctlConnectionSettings1.GSM = false;
             this.ctlConnectionSettings1.IP = null;
-            this.ctlConnectionSettings1.IsTCPSelected = true;
+            this.ctlConnectionSettings1.IsTCPSelected = false;
             this.ctlConnectionSettings1.Location = new System.Drawing.Point(10, 12);
             this.ctlConnectionSettings1.Name = "ctlConnectionSettings1";
             this.ctlConnectionSettings1.Size = new System.Drawing.Size(292, 396);
             this.ctlConnectionSettings1.TabIndex = 2;
             this.ctlConnectionSettings1.TCPPort = "0";
             // 
+            // ctlMeters1
+            // 
+            this.ctlMeters1.AddressMeter = "1";
+            this.ctlMeters1.AddressParam = 1;
+            this.ctlMeters1.ChannelParam = 1;
+            this.ctlMeters1.EnableAuxilary = true;
+            this.ctlMeters1.EnableCurrent = true;
+            this.ctlMeters1.Enabled = false;
+            this.ctlMeters1.EnableDaily = true;
+            this.ctlMeters1.EnableHalfs = false;
+            this.ctlMeters1.EnableMonthly = true;
+            this.ctlMeters1.Location = new System.Drawing.Point(308, 12);
+            this.ctlMeters1.Name = "ctlMeters1";
+            this.ctlMeters1.PasswordMeter = "111111";
+            this.ctlMeters1.Size = new System.Drawing.Size(898, 268);
+            this.ctlMeters1.TabIndex = 1;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1112, 416);
+            this.ClientSize = new System.Drawing.Size(1206, 416);
             this.Controls.Add(this.ctlConnectionSettings1);
             this.Controls.Add(this.ctlMeters1);
             this.Name = "Main";
